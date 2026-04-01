8f302ae4b05b3b09d09ff4b430787d5a0fb63208
Pedro Leite <pedroleite3010@hotmail.com>
2026-04-01 13:54:10 -0300

    actual creation of player class and constructor

--- Player.java
+++ Player.java
@@ -1,3 +1,14 @@
 public class Player {
-    
+
+    //character atributes
+    String name;
+    int life;
+    int points;
+
+    //constructor 
+    public Player(String name, int life, int points){
+        this.name = name;
+        this.life = life;
+        this.points = points;
+    }
 }
