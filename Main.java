0c5fb8f41bca4d63964d645d89545dcb98dfd3b4
Pedro Leite <pedroleite3010@hotmail.com>
2026-04-10 21:36:49 -0300

    im restarting this shit 😭😭😭

--- Main.java
+++ Main.java
@@ -1,20 +1,7 @@
 public class Main {
+
     public static void main (String [] args){
-        //room manager so we can change screens whenever we like
-        Room roomManager = new Room(0);
-
-        //if the player clicks the exit button, roomIndex will be < 0 and the game will close
-        //its the main loop condition to exist
-        while (roomManager.roomIndex > 0){
-            switch(roomManager.roomIndex){
-                case 1:
-                    System.out.println("CODEARENA: BATALHA DO CONHECIMENTO");
-                    System.out.println("Deseja começar o jogo? (y/n)");
-                    
-
-            }
-        }
-
+        
     }
-    
+
 }
--- Question.java
+++ Question.java
@@ -1,14 +1,3 @@
 public class Question {
 
-    QuestionBank[] questions = new QuestionBank[30];
-    String text;
-    String altText;
-    int altValue;
-
-    public Question(String text, String altText, int altValue){
-        this.text = text;
-        this.altText = altText;
-        this.altValue = altValue;
-    }
-    
 }
