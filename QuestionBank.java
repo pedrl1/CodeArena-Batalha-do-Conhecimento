4aab9984feb6862b4df310d862d35ad7ed2c192a
Pedro Leite <pedroleite3010@hotmail.com>
2026-04-24 21:40:31 -0300

    same as last commit but with a diff question bank

--- QuestionBank.java
+++ QuestionBank.java
@@ -1,47 +1,55 @@
 public class QuestionBank {
 
-    Question[] question = {
+    private Question[] questions = {
         //very easy questions
-        new Question(null, null, 1, 0),
-        new Question(null, null, 1, 0),
-        new Question(null, null, 1, 0),
-        new Question(null, null, 1, 0),
-        new Question(null, null, 1, 0),
-        new Question(null, null, 1, 0),
+        new Question("", new String[]{}, 1, 0),
+        new Question("", new String[]{}, 1, 0),
+        new Question("", new String[]{}, 1, 0),
+        new Question("", new String[]{}, 1, 0),
+        new Question("", new String[]{}, 1, 0),
+        new Question("", new String[]{}, 1, 0),
         
         //easy questions
-        new Question(null, null, 2, 0),
-        new Question(null, null, 2, 0),
-        new Question(null, null, 2, 0),
-        new Question(null, null, 2, 0),
-        new Question(null, null, 2, 0),
-        new Question(null, null, 2, 0),
+        new Question("", new String[]{}, 2, 0),
+        new Question("", new String[]{}, 2, 0),
+        new Question("", new String[]{}, 2, 0),
+        new Question("", new String[]{}, 2, 0),
+        new Question("", new String[]{}, 2, 0),
+        new Question("", new String[]{}, 2, 0),
         
         //medium questions
-        new Question(null, null, 3, 0),
-        new Question(null, null, 3, 0),
-        new Question(null, null, 3, 0),
-        new Question(null, null, 3, 0),
-        new Question(null, null, 3, 0),
-        new Question(null, null, 3, 0),
+        new Question("", new String[]{}, 3, 0),
+        new Question("", new String[]{}, 3, 0),
+        new Question("", new String[]{}, 3, 0),
+        new Question("", new String[]{}, 3, 0),
+        new Question("", new String[]{}, 3, 0),
+        new Question("", new String[]{}, 3, 0),
         
         //hard questions
-        new Question(null, null, 4, 0),
-        new Question(null, null, 4, 0),
-        new Question(null, null, 4, 0),
-        new Question(null, null, 4, 0),
-        new Question(null, null, 4, 0),
-        new Question(null, null, 4, 0),
+        new Question("", new String[]{}, 4, 0),
+        new Question("", new String[]{}, 4, 0),
+        new Question("", new String[]{}, 4, 0),
+        new Question("", new String[]{}, 4, 0),
+        new Question("", new String[]{}, 4, 0),
+        new Question("", new String[]{}, 4, 0),
         
         //very hard questions
-        new Question(null, null, 5, 0),
-        new Question(null, null, 5, 0),
-        new Question(null, null, 5, 0),
-        new Question(null, null, 5, 0),
-        new Question(null, null, 5, 0),
-        new Question(null, null, 5, 0)
+        new Question("", new String[]{}, 5, 0),
+        new Question("", new String[]{}, 5, 0),
+        new Question("", new String[]{}, 5, 0),
+        new Question("", new String[]{}, 5, 0),
+        new Question("", new String[]{}, 5, 0),
+        new Question("", new String[]{}, 5, 0)
     };
 
+    public Question[] getQuestions() {
+        return questions;
+    }
 
-}
+    public Question getQuestion(int index) {
+        if (index >= 0 && index < questions.length) {
+            return questions[index];
+        }
+        return null;
+    }
 }
